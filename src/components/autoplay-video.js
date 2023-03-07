7cbe25217b38dec92307a1a08c0cfd7f90a2bcb2
Andrew Rubin <arubin78@gmail.com>
2023-03-07 15:42:13 -0800

    fix(build): import stylesheet so webpack compiles it; auto-formatting

--- src/components/autoplay-video.js
+++ src/components/autoplay-video.js
@@ -1,11 +1,14 @@
 // packages
-import { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-import { useInView, useUserPrefs } from "@wethegit/react-hooks";
+import { useEffect, useRef, useState } from "react"
+import PropTypes from "prop-types"
+import { useInView } from "@wethegit/react-hooks"
 
 // utils
-import { classnames } from "../lib/classnames";
-import { makeHash } from "../lib/make-hash";
+import { classnames } from "../lib/classnames"
+import { makeHash } from "../lib/make-hash"
+
+// styles
+import "./autoplay-video.scss"
 
 export const AutoplayVideo = ({
   className,
@@ -13,44 +16,44 @@ export const AutoplayVideo = ({
   height,
   lazyLoadRootMargin,
   posterImg,
+  prefersReducedMotion,
   renderReducedMotionFallback,
   src,
   width,
 }) => {
-  const { prefersReducedMotion } = useUserPrefs();
   // "loaded" is a loose term. This just refers to whether the video's src tag has already been placed in the DOM:
-  const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false)
   const [setInViewRef, isInView] = useInView(
     { threshold: 0, rootMargin: lazyLoadRootMargin },
     false
-  );
+  )
   // gets us a unique ID for the video description:
   const descriptionID = description
     ? `autoplay-video-desc-${makeHash(description)}`
-    : null;
-  const videoRef = useRef();
+    : null
+  const videoRef = useRef()
 
   const pauseVideo = () => {
-    if (!videoRef.current) return;
-    videoRef.current.pause();
-  };
+    if (!videoRef.current) return
+    videoRef.current.pause()
+  }
 
   const playVideo = () => {
-    if (!videoRef.current) return;
-    videoRef.current.play();
-  };
+    if (!videoRef.current) return
+    videoRef.current.play()
+  }
 
   // Set a flag to load the content (video or fallback), based on its visibility.
   useEffect(() => {
-    if (isInView) setLoaded(true);
-  }, [isInView]);
+    if (isInView) setLoaded(true)
+  }, [isInView])
 
   // ensure the video does not continue to play when off-screen
   useEffect(() => {
-    if (!loaded) return;
-    if (isInView) playVideo();
-    else pauseVideo();
-  }, [isInView, loaded]);
+    if (!loaded) return
+    if (isInView) playVideo()
+    else pauseVideo()
+  }, [isInView, loaded])
 
   return (
     <div
@@ -59,21 +62,16 @@ export const AutoplayVideo = ({
       {...(width &&
         height && {
           style: {
-            "--aspect-ratio": `${
-              parseFloat((height / width).toFixed(4)) * 100
-            }%`,
+            "--aspect-ratio": `${parseFloat((height / width).toFixed(4)) * 100}%`,
           },
         })}
     >
-      {prefersReducedMotion && renderReducedMotionFallback() ? (
+      {prefersReducedMotion && renderReducedMotionFallback ? (
         renderReducedMotionFallback()
       ) : (
         <>
           {descriptionID && (
-            <p
-              id={descriptionID}
-              className="autoplay-video-util-visually-hidden"
-            >
+            <p id={descriptionID} className="autoplay-video-util-visually-hidden">
               {description}
             </p>
           )}
@@ -91,21 +89,22 @@ export const AutoplayVideo = ({
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
 AutoplayVideo.defaultProps = {
   description: "",
   lazyLoadRootMargin: "0px 0px 400px 0px",
-};
+}
 
 AutoplayVideo.propTypes = {
   className: PropTypes.string,
   description: PropTypes.string,
-  lazyLoadRootMargin: PropTypes.string,
+  height: PropTypes.number.isRequired,
+  lazyLoadRootMargin: PropTypes.string.isRequired,
   posterImg: PropTypes.string,
+  prefersReducedMotion: PropTypes.bool.isRequired,
   renderReducedMotionFallback: PropTypes.func,
   src: PropTypes.string,
   width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
-};
+}
